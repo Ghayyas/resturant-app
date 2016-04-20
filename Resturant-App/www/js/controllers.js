3ba5b4cba2476fda992a6da5602cda7cadfd83ec
Ghayyas <ghayyasmubashir@gmail.com>
2016-04-20 18:41:05 +0500

    app complete

--- Resturant-App/www/js/controllers.js
+++ Resturant-App/www/js/controllers.js
@@ -1,36 +1,9 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $firebaseArray, $state,facebookAuth) {
+.controller('AppCtrl', function($scope, $timeout, $firebaseArray, $state,facebookAuth,$location,$window) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
   $scope.loginData = {};
 
-  // Create the login modal that we will use later
-//   $ionicModal.fromTemplateUrl('templates/login.html', {
-//     scope: $scope
-//   }).then(function(modal) {
-//     $scope.modal = modal;
-//   });
-//  //   $scope.modal.show();
-
-//   // Triggered in the login modal to close it
-//   $scope.closeLogin = function() {
-//     $scope.modal.hide();
-//   };
-
-//   // Open the login modal
-//   $scope.login = function() {
-//     $scope.modal.show();
-//   };
-
-  // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     
    facebookAuth.flogin();
@@ -39,6 +12,13 @@ angular.module('starter.controllers', [])
 
    
   };
+       $scope.logout = function() {
+        localStorage.removeItem('fbToken');
+       // $route.reload(true);
+       $window.location.reload();
+
+        $location.path('/login');
+    }
 })
 
 .controller('homeCtrl', function($scope, refer, $firebaseArray) {
@@ -48,99 +28,7 @@ angular.module('starter.controllers', [])
     
     $scope.resturants = $firebaseArray(bc);
     
-    // console.log($scope.resturants);
     
-    // $scope.resturants.$add({
-    //     rName: "Kolachi",
-    //     rAddr: "Clifton 2 Dariya",
-    //     rRatting: 5,
-    //     rLogo: '',
-    //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu:[ 
-    //        { menu1: "Chicken Karhai : 200 Rs"},
-    //        {menu1: "Chicken Burger : 300 Rs"},
-    //        {menu1: "Haleem : 700 Rs"},
-    //        {menu1: "Qorma : 450 Rs"}
-    //     ]
-        
-    // })
-    //     $scope.resturants.$add({
-    //     rName: "Kolachi",
-    //     rAddr: "Clifton 2 Dariya",
-    //     rRatting: 5,
-    //     rLogo: '',
-    //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu:[ 
-    //        { menu1: "Chicken Karhai : 200 Rs"},
-    //        {menu1: "Chicken Burger : 300 Rs"},
-    //        {menu1: "Haleem : 700 Rs"},
-    //        {menu1: "Qorma : 450 Rs"}
-    //     ]
-        
-    // })
-    //     $scope.resturants.$add({
-    //     rName: "Kolachi",
-    //     rAddr: "Clifton 2 Dariya",
-    //     rRatting: 5,
-    //     rLogo: '',
-    //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu:[ 
-    //        { menu1: "Chicken Karhai : 200 Rs"},
-    //        {menu1: "Chicken Burger : 300 Rs"},
-    //        {menu1: "Haleem : 700 Rs"},
-    //        {menu1: "Qorma : 450 Rs"}
-    //     ]
-        
-    // })
-    //     $scope.resturants.$add({
-    //     rName: "Kolachi",
-    //     rAddr: "Clifton 2 Dariya",
-    //     rRatting: 5,
-    //     rLogo: '',
-    //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu:[ 
-    //        { menu1: "Chicken Karhai : 200 Rs"},
-    //        {menu1: "Chicken Burger : 300 Rs"},
-    //        {menu1: "Haleem : 700 Rs"},
-    //        {menu1: "Qorma : 450 Rs"}
-    //     ]
-        
-    // })
-    
-    //     $scope.resturants.$add({
-    //     rName: "dfdf",
-    //     rAddr: "dfdf",
-    //     rRatting: 5,
-    //     rLogo: '',
-    //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu:[ 
-    //        {"Chicken Karhai": "200 Rs"},
-    //        {"Chicken Burger": "300 Rs"},
-    //        {"Haleem": "700 Rs"},
-    //        {"Qorma": "450 Rs"}
-    //     ]
-        
-    // })
-    //     $scope.resturants.$add({
-    //     rName: "wetrf",
-    //     rAddr: "vsrer",
-    //     rRatting: 5,
-    //     rLogo: '',
-    //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu:[ 
-    //        {"Chicken Karhai": "200 Rs"},
-    //        {"Chicken Burger": "300 Rs"},
-    //        {"Haleem": "700 Rs"},
-    //        {"Qorma": "450 Rs"}
-    //     ]
-        
-    // })
     //     $scope.resturants.$add({
     //     rName: "Tooso",
     //     rAddr: "Bahurabad Near Sayalani Welfare",
@@ -148,12 +36,10 @@ angular.module('starter.controllers', [])
     //     rLogo: '',
     //     rDevFee: 500,
     //     rMinOrderAmount: 200,
-    //     rMenu: [{
-    //        "Chicken Karhai": "2000 Rs",
-    //        "Chicken Burger": "3000 Rs",
-    //        "Haleem": "70 Rs",
-    //        "Qorma": "40 Rs"
-    //     }]
+    //     rMenu: [{ menu1: "Chicken Karhai : 200 Rs"},
+    //         {menu1: "Chicken Burger : 300 Rs"},
+    //         {menu1: "Haleem : 700 Rs"},
+    //         {menu1: "Qorma : 450 Rs"}]
         
     // })
     //     $scope.resturants.$add({
@@ -162,13 +48,10 @@ angular.module('starter.controllers', [])
     //     rRatting: 3,
     //     rLogo: '',
     //     rDevFee: 100,
-    //     rMinOrderAmount: 290,
-    //     rMenu: [{
-    //        "Chicken Karhai": "20 Rs",
-    //        "Chicken Burger": "3000 Rs",
-    //        "Haleem": "7000 Rs",
-    //        "Qorma": "450 Rs"
-    //     }]
+    //   rMenu: [{ menu1: "Chicken Karhai : 200 Rs"},
+    //         {menu1: "Chicken Burger : 300 Rs"},
+    //         {menu1: "Haleem : 700 Rs"},
+    //         {menu1: "Qorma : 450 Rs"}]
         
     // })
     //     $scope.resturants.$add({
@@ -178,12 +61,10 @@ angular.module('starter.controllers', [])
     //     rLogo: '',
     //     rDevFee: 500,
     //     rMinOrderAmount: 200,
-    //     rMenu: [{
-    //        "Chicken Karhai": "200 Rs",
-    //        "Chicken Burger": "300 Rs",
-    //        "Haleem": "700 Rs",
-    //        "Qorma": "450 Rs"
-    //     }]
+    //    rMenu: [{ menu1: "Chicken Karhai : 200 Rs"},
+    //         {menu1: "Chicken Burger : 300 Rs"},
+    //         {menu1: "Haleem : 700 Rs"},
+    //         {menu1: "Qorma : 450 Rs"}]
         
     // })
     //     $scope.resturants.$add({
@@ -192,38 +73,17 @@ angular.module('starter.controllers', [])
     //     rRatting: 1,
     //     rLogo: '',
     //     rDevFee: 500,
-    //     rMinOrderAmount: 200,
-    //     rMenu: [{
-    //        "Chicken Karhai": "200 Rs",
-    //        "Chicken Burger": "300 Rs",
-    //        "Haleem": "700 Rs",
-    //        "Qorma": "450 Rs"
-    //     }]
+    //     rMenu: [
+    //         { menu1: "Chicken Karhai : 200 Rs"},
+    //         {menu1: "Chicken Burger : 300 Rs"},
+    //         {menu1: "Haleem : 700 Rs"},
+    //         {menu1: "Qorma : 450 Rs"}
+    //         ]
         
     // })
- 
-    
-  $scope.playlists = [
-    { title: 'Kolachi', id: 1 },
-    { title: 'Tooso', id: 2 },
-    { title: 'Mr.Burgur', id: 3 },
-    { title: 'KFC', id: 4 },
-    { title: 'Mac Dolands', id: 5 }
- 
-  ];
 })
-.controller('settingCtrl',function($scope,$state,$location,$window){
-   // var token = localStorage.getItem('fbToken');
-      $scope.logout = function() {
-        localStorage.removeItem('fbToken');
-       // $route.reload(true);
-       $window.location.reload();
+ 
 
-        $location.path('/login');
-    }
-    
-
-})
 .controller('selectedCtrl', function($scope, $stateParams, refer,$firebaseObject,$firebaseArray) {
      
      $scope.bc = refer.child("resturants");
@@ -241,8 +101,7 @@ angular.module('starter.controllers', [])
          localStorage.setItem('orderID',id);
          console.log("ID is ",id);
      }
-     
-//    var abc = JSON.parse($scope.arr);
+
     
    
    
